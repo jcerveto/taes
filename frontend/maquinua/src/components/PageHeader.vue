df136b56afa4feab765c0c1d5865ee45b668f6aa
jcerveto <joan.cer.ser@gmail.com>
2024-02-21 00:45:12 +0100

    Prototipo inicial web ejecutable

---
+++ frontend/maquinua/src/components/PageHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/filter">Filter</router-link>
+        <router-link to="/products">View products</router-link>
+        <router-link to="/machines">View machines</router-link>
+        <router-link to="/about">About</router-link>
+        <button id="toggleDarkMode">Dark Mode</button>
+    </nav>
+</template>
+
+
+<style scoped>
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    background-color: #f1f1f1;
+    padding: 50px;
+
+}
+
+nav a {
+    text-decoration: none;
+    padding: 10px;
+    color: black;   
+}
+
+nav a:hover {
+    background-color: #ddd;
+}
+
+</style>
---
+++ frontend/maquinua/src/router.js
@@ -0,0 +1,42 @@
+// router.js
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/HomePage.vue';
+import About from '@/views/AboutPage.vue';
+import Error404 from '@/views/Error404Page.vue';
+import ProductPricePage from '@/views/ProductPricesPage.vue';
+import MachinesDistributionPage from '@/views/MachinesDistributionPage.vue';
+import FilterPage from '@/views/FilterPage.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
+  {
+    path: '/products',
+    component: ProductPricePage,
+  },
+  {
+    path: '/machines',
+    component: MachinesDistributionPage,
+  },
+  {
+    path: '/filter',
+    component: FilterPage,
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: Error404,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
