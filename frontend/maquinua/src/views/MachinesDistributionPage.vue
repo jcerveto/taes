1f32dacd52906066409294bcfb296524a7781bfd
Jose Megía Guillén <jmgm62@gcloud.ua.es>
2024-02-25 17:44:51 +0100

    Se muestra el mapa con marcadores

--- frontend/maquinua/src/views/MachinesDistributionPage.vue
+++ frontend/maquinua/src/views/MachinesDistributionPage.vue
@@ -1,8 +1,46 @@
+<template>
+  <div>
+    <h1>Mapa de la Universidad de Alicante</h1>
+    <div id="map"></div>
+  </div>
+</template>
+
 <script>
-//import TableFromCsv from '@/components/TableFromCsv.vue';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Importa la imagen del marcador
+import miMarcadorIcon from './mi_marcador.png';
+
+export default {
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      const uaCoordinates = [38.384, -0.513];
+      const map = L.map('map').setView(uaCoordinates, 19);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors',
+      }).addTo(map);
+
+      // Uso un marcador llamado mi_marcador.png
+      L.marker(uaCoordinates, {
+        icon: L.icon({
+          iconUrl: miMarcadorIcon,
+          iconSize: [15, 35],
+          iconAnchor: [22, 94],
+          popupAnchor: [-3, -76],
+        }),
+      }).addTo(map).bindPopup('Universidad de Alicante').openPopup();
+    },
+  },
+};
 </script>
 
-<template>
-    <h1>Machines Distribution</h1>
-    <!--<TableFromCsv csvPath="/distribution.csv" title="Teika's distribution" />-->
-</template>
+<style>
+#map {
+  height: 600px;
+}
+</style>
