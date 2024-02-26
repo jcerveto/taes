4526585405a628650459f4c1c5f37c76f3479e71
Nicolás Martín Morcillo <nmm144@alu.ua.es>
2024-02-26 18:38:24 +0100

    Primeras cositas

--- frontend/maquinua/src/components/PageHeader.vue
+++ frontend/maquinua/src/components/PageHeader.vue
@@ -5,10 +5,35 @@
         <router-link to="/products">View products</router-link>
         <router-link to="/machines">View machines</router-link>
         <router-link to="/about">About</router-link>
+        <button v-if="!isLoggedIn" @click="redirectToSignIn">Sign In</button>
         <button id="toggleDarkMode">Dark Mode</button>
+        <img v-if="isLoggedIn" src="ruta/a/la/foto-de-perfil.jpg" alt="User Profile" id="userProfile">
     </nav>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  methods: {
+    redirectToSignIn() {
+      // Aquí puedes redirigir usando Vue Router
+      this.$router.push('/signin'); // Reemplaza '/signin' con la ruta a tu página de inicio de sesión
+    },
+    checkLoggedIn() {
+      // Lógica para comprobar si el usuario está autenticado
+      // Podría ser una llamada a una API, verificación de tokens, etc.
+      // Aquí lo simulamos con un simple ejemplo
+      const userLoggedIn = /* Aquí tu lógica para comprobar si está autenticado */
+      this.isLoggedIn = userLoggedIn;
+    },
+  },
+  
+};
+</script>
 
 <style scoped>
 
