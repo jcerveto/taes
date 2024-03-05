28dc9ef88c0413ea9e513bcf253a081d7106734c
jcerveto <joan.cerveto.serrano@gmail.com>
2024-03-05 14:22:37 +0100

    Carpeta Server a falta de corregir imports

---
+++ backend/main.js
@@ -0,0 +1,88 @@
+import express from 'express';
+import cors from 'cors';
+
+
+import * as db from './services/db';
+import { User } from './model/User';
+
+
+const app = express();
+const PORT = 3000;
+
+// Allow CORS requests from your Vue.js application's URL
+app.use(cors({ origin: 'http://localhost:8080' })); // Replace with your Vue.js app's URL
+
+
+app.get('/', async (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await db.getAllUsers();
+        // ver formato de respuesta, creo qye es Array<User>. Checkear q es correcto al hacer res.json()
+        // COmprobarlo en todos los endpoints (funciones de express)
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.read(req.params.email)
+        res.json(user.json());
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const cleanUser = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        };
+        const user = await db.createUser(cleanUser);
+        res.json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+app.put('/users/:id', async (req, res) => {
+    try {
+        const cleanUser = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        };
+        const user = await db.updateUser(req.params.id, cleanUser);
+        res.json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await db.deleteUser(req.params.id);
+        res.json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`)
+})
+
+
