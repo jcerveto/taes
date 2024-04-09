a8e34b00ff4cdd7408086c0afb67cb372f1954ca
cacs2-ua <cacs2@alu.ua.es>
2024-04-09 19:11:37 +0200

    Support Page 31.0 Filters Done 7.0

--- frontend/maquinua/src/views/SupportPage.vue
+++ frontend/maquinua/src/views/SupportPage.vue
@@ -1,29 +1,41 @@
 <template>
    <div>
     <h1>Support Page</h1>
+    <br>
+    <router-link to="/" class="allign-right-home">Go to Home</router-link>
+    <br>
     <div class="filter-container">
-      <!-- Dropdown filter for machine type with class added -->
+      
       <select v-model="selectedMachineType" class="filter-dropdown">
-        <option value="">All Types</option>
+        <option value="">Todos los tipos</option>
         <option value="MIXTA">MIXTA</option>
         <option value="CAFETERA">CAFETERA</option>
         <option value="BEBIDAS FRIAS">BEBIDAS FRIAS</option>
         <option value="COMIDA SALUDABLE">COMIDA SALUDABLE</option>
       </select>
-      <!-- Dropdown filter for building with class added -->
+
       <select v-model="selectedBuilding" class="filter-dropdown">
-        <option value="">All Buildings</option>
+        <option value="">Todos los edificios</option>
         <option v-for="building in buildingOptions" :key="building" :value="building">{{ building }}</option>
       </select>
-      <!-- Input for specific product with class added -->
-      <input type="text" v-model="productFilter" placeholder="Producto concreto" class="filter-input">
-      <!-- Input for Precio mínimo with class added -->
-      <input type="number" v-model.number="minPrice" placeholder="Precio mínimo" step="0.10" class="filter-input">
-      <!-- Input for Precio máximo with class added -->
-      <input type="number" v-model.number="maxPrice" placeholder="Precio máximo" step="0.10" class="filter-input">
+      <div class="input-container">
+        <input type="text" v-model="productFilter" class="filter-input">
+        <label for="productFilter">Producto concreto</label>
+      </div>
+
+      <div class="input-container">
+        <input type="number" v-model.number="minPrice" step="0.10" min="0" class="filter-input">
+        <label for="minPrice">Precio mínimo</label>
+      </div>
+
+      <div class="input-container">
+        <input type="number" v-model.number="maxPrice" step="0.10" min="0" class="filter-input">
+        <label for="maxPrice">Precio máximo</label>
+      </div>
+
     </div>
     <br>
-    <router-link to="/">Go to Home</router-link>
+    
 
     <div v-for="(building, index) in filteredBuildings" :key="index">
       <details>
@@ -53,7 +65,6 @@
                     <th> Nombre del producto</th>
                     <th> Precio del producto</th>
                   </tr>
-                  <!-- Looping through each product and price pair -->
                   <tr v-for="(product, index) in machine.lista_productos" :key="index">
                     <td contenteditable="true" @blur="updateProductName(machine.id, index, $event.target.innerText)">{{ product }}</td>
                     <td contenteditable="true" @blur="updateProductPrice(machine.id, index, $event.target.innerText)" @input="validatePrice($event)">{{ machine.lista_precios[index].toFixed(2) }}</td>
@@ -77,8 +88,8 @@ export default {
       selectedMachineType: '',
       selectedBuilding: '',
       productFilter: '',
-      minPrice: null, // Holds the minimum price entered by the user
-      maxPrice: null, // Holds the maximum price entered by the user
+      minPrice: null, 
+      maxPrice: null, 
     };
   },
   computed: {
@@ -87,14 +98,14 @@ export default {
       return Array.from(buildingSet);
     },
     filteredBuildings() {
-      // Filters buildings based on the selected machine type, building, product filter, and price filters
+  
       if (this.selectedBuilding) {
-        // Filter for a specific selected building
+        
         return this.buildings.filter(building => 
           building.name === this.selectedBuilding && this.filteredMachines(building).length > 0
         );
       } else {
-        // No specific building selected, apply other filters
+
         return this.buildings.filter(building => this.filteredMachines(building).length > 0);
       }
     },
@@ -120,11 +131,10 @@ export default {
       this.buildings = Object.values(buildingMap);
     },
     filteredMachines(building) {
-      // Filters machines within each building based on the applied filters
       return building.machines.filter(machine => {
         const matchesType = !this.selectedMachineType || machine.type === this.selectedMachineType;
         const matchesProduct = !this.productFilter || machine.lista_productos.some(product =>
-          product.toLowerCase().startsWith(this.productFilter.toLowerCase())); // Adjusted to use startsWith
+          product.toLowerCase().startsWith(this.productFilter.toLowerCase())); 
         const matchesMinPrice = !this.minPrice || machine.lista_precios.some(price => price >= this.minPrice);
         const matchesMaxPrice = !this.maxPrice || machine.lista_precios.some(price => price <= this.maxPrice);
 
@@ -143,24 +153,16 @@ export default {
 
 
     updateProductName(machineId, productIndex, newName) {
-    // Find machine by id and update the product name
-    // This is a placeholder; actual implementation will depend on how you manage state and backend updates
     console.log(`Update product name for machine ${machineId}, product index ${productIndex}, new name: ${newName}`);
-    // Example: this.machines.find(machine => machine.id === machineId).lista_productos[productIndex] = newName;
-    // You would typically make an API call here to update the backend
+    
     },
     updateProductPrice(machineId, productIndex, newPrice) {
     const price = parseFloat(newPrice).toFixed(2);
     if (!isNaN(price)) {
-      // Find machine by id and update the product price
-      // This is a placeholder; actual implementation will depend on your state management and backend update mechanism
       console.log(`Update product price for machine ${machineId}, product index ${productIndex}, new price: ${price}`);
-      // Example: this.machines.find(machine => machine.id === machineId).lista_precios[productIndex] = parseFloat(price);
-      // You would typically make an API call here to update the backend
     }
    },
   validatePrice(event) {
-    // Prevent input if it's not a number or if it doesn't fit the price format
     const input = event.target.innerText;
     const validFormat = /^\d+(\.\d{0,2})?$/;
     if (!validFormat.test(input)) {
@@ -182,7 +184,7 @@ export default {
   <style scoped>
 
     div.table-container {
-  margin-top: 1em; /* Adds space between vending machine details */
+  margin-top: 1em; 
 }
 
 details > summary {
@@ -197,13 +199,7 @@ table {
   margin-bottom: 2em;
 }
 
-/*
-.myGood {
-  padding: 0.5em;
-  border: 1px solid #ddd;
-  text-align: left;
-}
-*/
+
 
 th, td {
   padding: 0.5em;
@@ -211,49 +207,49 @@ th, td {
   text-align: center;
 }
 
-/* MIXTA style - odd rows darker, even rows lighter */
+
 .table-mixta tr:nth-child(odd) {
-  background-color: #FFD6DD; /* Darker pink for odd rows */
+  background-color: #FFD6DD; 
 }
 .table-mixta tr:nth-child(even), .table-mixta .productos-header {
-  background-color: #FFC0CB; /* Lighter pink for even rows and productos header */
+  background-color: #FFC0CB; 
 }
 
-/* CAFETERA style - odd rows darker, even rows lighter */
+
 .table-cafetera tr:nth-child(odd) {
-  background-color: #D2B48C; /* Darker brown for odd rows */
+  background-color: #D2B48C; 
 }
 .table-cafetera tr:nth-child(even), .table-cafetera .productos-header {
-  background-color: #E4CDA2; /* Lighter brown for even rows and productos header */
+  background-color: #E4CDA2; 
 }
 
-/* BEBIDAS FRIAS style - odd rows darker, even rows lighter */
+
 .table-bebidas-frias tr:nth-child(odd) {
-  background-color: #ADD8E6; /* Darker blue for odd rows */
+  background-color: #ADD8E6; 
 }
 .table-bebidas-frias tr:nth-child(even), .table-bebidas-frias .productos-header {
-  background-color: #BFEFFF; /* Lighter blue for even rows and productos header */
+  background-color: #BFEFFF; 
 }
 
-/* COMIDA SALUDABLE style - odd rows darker, even rows lighter */
+
 .table-comida-saludable tr:nth-child(odd) {
-  background-color: #90EE90; /* Darker green for odd rows */
+  background-color: #90EE90; 
 }
 .table-comida-saludable tr:nth-child(even), .table-comida-saludable .productos-header {
-  background-color: #98FB98; /* Lighter green for even rows and productos header */
+  background-color: #98FB98; 
 }
 
-/* Adjusting the "Productos" header to span two columns */
+
 .productos-header th {
   text-align: center;
   font-weight: bold;
 }
 
-/* Add styles for the filter section */
+
 .filter-container {
   display: flex;
-  gap: 8px; /* Adjust the space between filters */
-  margin-bottom: 16px; /* Space below the filter section */
+  gap: 8px; 
+  margin-bottom: 16px; 
   justify-content: center;
 }
 
@@ -261,25 +257,45 @@ th, td {
 .filter-container input {
   padding: 8px;
   border: 1px solid #cccccc;
-  border-radius: 4px; /* Rounded corners */
-  outline: none; /* Removes the default focus outline */
+  border-radius: 4px; 
+  outline: none; 
 }
 
 .filter-container select:focus,
 .filter-container input:focus {
-  border-color: #666666; /* Darker border on focus */
+  border-color: #666666; 
 }
 
-/* Style the dropdown to match the provided style */
 .filter-container select {
-  -webkit-appearance: none; /* Removes default arrow in WebKit browsers */
-  -moz-appearance: none; /* Removes default arrow in Firefox */
-  appearance: none; /* Removes default arrow */
-  background-size: 12px; /* Adjust as needed */
+  background-size: 12px; 
   cursor: pointer;
 }
 
-/* You may need to adjust the URL to the correct path of your dropdown arrow image */
+.allign-right-home {
+  position: absolute;
+  right: 0;
+  top: 7.5%;
+}
+
+.input-container {
+  position: relative;
+  margin-bottom: 20px; 
+}
+
+.input-container input {
+  width: 100%; 
+  height: 157%;
+}
+
+.input-container label {
+  position: absolute;
+  top: -25px; 
+  left: 0;
+  transform: translateX(35%);
+  font-size: 1.0em; 
+  font-weight: bold;
+}
+
 
 
 </style>
