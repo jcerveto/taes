25d08bdfaf15b4b1e27c45d7162f298a93220624
jcerveto <joan.cerveto.serrano@gmail.com>
2024-04-11 00:09:05 +0200

    Preparado para deploy a gh-pages

--- frontend/maquinua/vue.config.js
+++ frontend/maquinua/vue.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from '@vue/cli-service';
 
 export default defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/taes/'
+  : '/'
 });
