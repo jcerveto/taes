30bc94d95b33585d23f5a4ed7baccb40d45b43a3
Carlos Guirao <cgmg3@alu.ua.es>
2024-04-28 22:26:24 +0200

    landing page

--- frontend/maquinua/src/views/LandingPage.vue
+++ frontend/maquinua/src/views/LandingPage.vue
@@ -1,24 +1,59 @@
-<template>
+ <template>
   <div class="landing-page">
     <div class="content">
-      <p class="textotop">Mi texto encima del botón</p>
-      <button @click="redirigir">Haz clic aquí</button>
+      <p class="textotop">Unete a la familia <span class="maquinUA">MaquinUA</span> y disfruta de todo lo que te ofrecen las maquinas expededoras</p>
+      <button class="botontop" @click="redirigirregister">Unete</button>
       
+        
+
     </div>
     <img class="imagen-desvanecida" src=..\assets\LogoMaquinUA.jpg alt="Mi imagen">
   </div>
+     <div class="container">
+        <div class="column">
+            <img class="img" src=..\assets\lupa.png >
+            <p>¿Que maquina escojo?</p>
+         <p class="texto">Estás indeciso en que maquina expendedora elegir? busca en nuestro mapa</p>
+         <button @click="redirigirmap">Mapa</button>
+             </div>
+        <div class="column">
+         <img class="img" src=..\assets\exclamacion.png >
+            <p>¿Error en la maquina?</p>
+         <p class="texto">Has visto algun error en nuestras maquinas expendedoras? informanos del error </p>
+         <button @click="redirigirincid">Incidencia</button>
+        </div>
+        <div class="column">
+         <img class="img" src=..\assets\lupa.png >
+            <p>¿Que prefieres?</p>
+         <p class="texto">Hay algun producto que te interese en especial? aqui encontraras todos los que hay</p>
+         <button @click="redirigirprod">Productos</button>
+         </div>
+      </div>
 </template>
 <script>
+    
    export default {
   methods: {
-    redirigir() {
+    redirigirregister() {
+      this.$router.push('/register');
+    },
+    redirigirmap() {
       this.$router.push('/');
+    },
+    redirigirincid() {
+      this.$router.push('/incidents');
+    },
+     redirigirprod() {
+      this.$router.push('/products');
     }
-  }
+}
 }
 </script>
 
 <style scoped>
+   
+
+
 .landing-page {
   display: flex;
   justify-content: space-between;
@@ -36,25 +71,74 @@ button {
   margin-top: 15px;
   padding: 10px 20px;
   font-size: 16px;
-  border: none;
-  border-radius: 5px;
+  border: black;
+  border-radius: 30px;
   background-color: #7fcaad;
   color: white;
   cursor: pointer;
 }
-    .textotop {
-  font-size: 20px; /* Tamaño del texto */
-  font-weight: bold; /* Texto en negrita */
-  color: #fff; /* Color del texto */
+    .botontop {
+     margin-left: 350px;
+     margin-top: 50px;
+    margin-right: 350px;
+    font-size: 20px;
+    padding: 15px 30px;
 }
-    .imagen-desvanecida {
-  width: 300px; /* Ajusta el tamaño de la imagen */
-  height: auto;
- overflow: hidden;
-   clip-path: ellipse(50% 70% at 50% 50%);
+    .maquinUA{
+    color: #7fcaad
+    }
+    .textotop {
+    margin-top: 30px;
+    margin-right: 100px;
+    margin-left: 15px;
+    text-align: left;
+  font-size: 30px; /* Tamaño del texto */
+  font-weight: bold; /* Texto en negrita */
   
 }
+    .imagen-desvanecida {
+    width: 300px; /* Ajusta el tamaño de la imagen */
+    height: 300px;
+    verflow: hidden;
+    margin-right: 300px;
+    display: inline-block;
+    border-radius: 9999999999px;
+    
+    
+}
+   .imagen-desvanecida::after {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  box-shadow: inset 0 0 0 10px rgba(0, 0, 0, 0.5);
+}
+    .container {
+  display: flex;
+  justify-content: space-between;
+}
 
+    .column {
+  flex: 1;
+  padding: 10px;
+  box-sizing: border-box;
+  font-size: 20px; /* Tamaño del texto */
+  font-weight: bold; /* Texto en negrita */
+  color: #000; /* Color del texto */
+}
+    .texto{
+   font-size: 16px;
+  font-weight: normal;
+  color: #000000; /* Este es el color negro, pero puedes cambiarlo a lo que prefieras */
+}
+    .img{
+    width: 40px; /* Ajusta el tamaño de la imagen */
+    height: 40px;
+    opacity: 0.3;
+}
 
 
 </style>
