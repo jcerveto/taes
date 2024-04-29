cc1359a1eac58c8a550b5e1fd487e8e083dce857
cacs2-ua <cacs2@alu.ua.es>
2024-04-29 17:59:03 +0200

    main.js redundance deleted for Support

--- backend/src/main.js
+++ backend/src/main.js
@@ -16,6 +16,10 @@ app.use(cookieParser());
 
 const PORT = 3000;
 
+
+
+
+
 app.use(cors({
     origin: 'http://localhost:8080', // Your Vue.js application's URL
     credentials: true,
@@ -244,52 +248,7 @@ app.delete('/users/:id', async (req, res) => {
     }
 });
 
-app.post('/update-machine', async (req, res) => {
-    try {
-        const updatedMachine = req.body;
-        const filePath = path.join(path.resolve(), 'public/maquinas.json');
 
-        const data = await fs.promises.readFile(filePath, 'utf8');
-        let machines = JSON.parse(data);
-        const index = machines.findIndex(m => m.id === updatedMachine.id);
-        
-        if (index === -1) {
-            return res.status(404).json({ message: 'Machine not found' });
-        }
-        
-        machines[index] = updatedMachine;
-
-        await fs.promises.writeFile(filePath, JSON.stringify(machines, null, 2), 'utf8');
-        res.json({ message: 'Machine updated successfully', updatedMachine });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.put('/update-machine', async (req, res) => {
-    const { id, newProduct, newPrice } = req.body;
-    const filePath = path.join(path.resolve(), 'public/maquinas.json');
-
-    try {
-        const data = await fs.promises.readFile(filePath, 'utf8');
-        let machines = JSON.parse(data);
-        const machine = machines.find(machine => machine.id === id);
-
-        if (!machine) {
-            return res.status(404).json({ message: 'Machine not found' });
-        }
-
-        machine.lista_productos.push(newProduct);
-        machine.lista_precios.push(parseFloat(newPrice));
-
-        await fs.promises.writeFile(filePath, JSON.stringify(machines, null, 2), 'utf8');
-        res.json({ message: 'Product added successfully', machine });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error.message });
-    }
-});
 
 app.listen(PORT, () => {
     console.log(`app listening on port ${PORT}`)
